1f07018dac7154106f5db2903008e9e38dbc4697
2017192 <u2017192@giki.edu.pk>
2020-11-20 20:26:33 +0500

    text_changes

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Changes
+          Changes_made
         </a>
       </header>
     </div>
--- my-app/src/App.test.js
+++ my-app/src/App.test.js
@@ -3,6 +3,6 @@ import App from "./App";
 
 test("renders learn react link", () => {
   render(<App />);
-  const linkElement = screen.getByText(/Changes/i);
+  const linkElement = screen.getByText(/Changes_made/i);
   expect(linkElement).toBeInTheDocument();
 });
