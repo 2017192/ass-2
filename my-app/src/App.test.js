b1e684704c78a6bfd2de7b3c81dbf3cba4c7874d
2017192 <74494827+2017192@users.noreply.github.com>
2020-11-22 16:17:49 +0500

    Update App.test.js

--- my-app/src/App.test.js
+++ my-app/src/App.test.js
@@ -3,6 +3,6 @@ import App from "./App";
 
 test("renders learn react link", () => {
   render(<App />);
-  const linkElement = screen.getByText(/Huzaifa_2017192/i);
+  const linkElement = screen.getByText(/Huzaifa_2017192_Testing_Deployment/i);
   expect(linkElement).toBeInTheDocument();
 });
